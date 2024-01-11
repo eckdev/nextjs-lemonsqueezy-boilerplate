f48dd1f667c61e644c8929b7449475c96f5915ae
eckdev <eyupcankayadarcin@hotmail.com>
2024-01-11 11:41:34 +0300

    remove unnecessary fields

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,8 @@
 
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { Session, getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
-import Script from "next/script";
-import Button from "./button";
-import { LemonsqueezyClient } from "lemonsqueezy.ts";
-import { retrieveOrder, listAllOrders } from "lemonsqueezy.ts/order";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions) as Session;
@@ -15,11 +10,6 @@ const Dashboard = async () => {
     redirect("/login");
   }
   
-  const key = process.env.LEMONSQUEEZY_API_KEY as string
-  const orders = await listAllOrders({
-    apiKey: key
-  });
-  const isSubscribed = orders.data.find(x => x.attributes.user_email === session.user?.email && x.attributes.status === 'paid')
   return (
     <>
       <span>dashboard</span>
