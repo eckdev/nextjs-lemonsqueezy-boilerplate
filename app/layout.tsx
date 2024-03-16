038b62b048a80fc760e3e4b1fa01b72fa9f94e88
eckdev <eyupcankayadarcin@hotmail.com>
2024-03-16 21:43:13 +0300

    buy me coffee added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import "./globals.css";
 import { sfPro, inter } from "./fonts";
 import { Toaster } from "@/components/ui/toast/toaster";
@@ -28,6 +29,7 @@ export default async function RootLayout({
         {children}
         <Toaster />
         <script src="https://assets.lemonsqueezy.com/lemon.js" defer></script>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="eckdev" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </body>
     </html>
   );
