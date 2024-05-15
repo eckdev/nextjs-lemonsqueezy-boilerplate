7e74fe06318d822c3ca5cff47caab67ec7c63a45
eckdev <eyupcankayadarcin@hotmail.com>
2024-05-15 15:04:38 +0300

    update logo

--- app/(auth)/login/forgot-password/page.tsx
+++ app/(auth)/login/forgot-password/page.tsx
@@ -1,22 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import ForgotPasswordForm from "@/components/forms/forgot-password-form";
+import { Logo } from "@/components/logo";
 
 export default function ForgotPassword() {
   return (
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-background px-4 py-6 pt-8 text-center sm:px-16">
-          <Link href="/">
-            <Image
-              src="/clogo.png"
-              priority
-              alt="Saas Boilerplate Logo"
-              className="mb-4"
-              width={200}
-              height={125}
-            />
-          </Link>
-          <h3 className="text-xl font-semibold">Password Reset</h3>
+          <Logo />
+          <h3 className="text-xl font-semibold pt-4">Password Reset</h3>
           <p className="text-sm text-gray-500">
             Enter your email to receive a reset link
           </p>
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,22 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import RegisterForm from "@/components/forms/register-form";
+import { Logo } from "@/components/logo";
 
 export default function Register() {
   return (
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-border shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-border bg-background px-4 py-6 pt-8 text-center sm:px-16">
-          <Link href="/">
-            <Image
-              src="/clogo.png"
-              priority
-              alt="Saas Boilerplate Logo"
-              className="mb-4"
-              width={200}
-              height={125}
-            />
-          </Link>
-          <h3 className="text-xl font-semibold">Sign Up</h3>
+        <Logo />
+          <h3 className="text-xl font-semibold pt-4">Sign Up</h3>
           <p className="text-sm text-muted-foreground">
             Create an account with your email and password
           </p>
